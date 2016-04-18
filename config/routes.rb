31dc9262721cc3e0dba58ccb3ed9415b71200d87
jaymuk <jaymuk.business@gmail.com>
2016-04-18 14:22:33 +0100

    built users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :sessions => "sessions", :registrations => "registrations"}
+
+  get "/my_current_user" => "users#my_current_user"
+  match 'users/:id' => 'users#update_user', via: [:patch]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
